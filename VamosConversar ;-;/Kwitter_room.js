c510eb9f4632af649ea942f59945b4dfc93539c4
ViniciusPereira11 <117477222+ViniciusPereira11@users.noreply.github.com>
2023-01-30 19:26:28 -0400

    Add files via upload

---
+++ VamosConversar ;-;/Kwitter_room.js
@@ -0,0 +1,9 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyAdLqw2SLivfTPhlD1SLjVc5SP_4RLywrY",
+    authDomain: "projetovamosconversar-fcc03.firebaseapp.com",
+    projectId: "projetovamosconversar-fcc03",
+    storageBucket: "projetovamosconversar-fcc03.appspot.com",
+    messagingSenderId: "1074573976545",
+    appId: "1:1074573976545:web:b261f935f833da1a1b68a0",
+    measurementId: "G-1RLJYC1PVD"
+  };
